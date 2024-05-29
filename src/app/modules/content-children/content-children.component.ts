437dbee0c40a2208fd66638a576a30cab40d23a2
Sharizan Redzuan <sharizan_81@yahoo.com>
2024-05-29 16:03:41 +0800

    Added content children component

---
+++ src/app/modules/content-children/content-children.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-children',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-children.component.html',
+  styleUrl: './content-children.component.css'
+})
+export class ContentChildrenComponent {
+
+}
